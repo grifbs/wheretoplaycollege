54e290ac830c39b1a6d9ece0c0c6834811a28ecf
grifbs <reedbandb@gmail.com>
2025-06-02 11:21:46 -0400

    Update card.js

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,1 +1,14 @@
-export const Card = (props) => <input {...props} />;
+import React from 'react';
+
+const Card = ({ children, className = '', ...props }) => {
+  return (
+    <div
+      className={`bg-white rounded shadow-md p-4 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
