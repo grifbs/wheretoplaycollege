2fe40c0c40cc5237d8b071baf64c1031723ebd41
grifbs <reedbandb@gmail.com>
2025-06-02 11:29:59 -0400

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,11 +1,9 @@
+// src/components/ui/card.js
 import React from 'react';
 
 const Card = ({ children, className = '', ...props }) => {
   return (
-    <div
-      className={`bg-white rounded shadow-md p-4 ${className}`}
-      {...props}
-    >
+    <div className={`bg-white rounded shadow-md p-4 ${className}`} {...props}>
       {children}
     </div>
   );
