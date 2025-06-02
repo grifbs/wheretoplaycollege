04b0acf657d47e884b39e3ce7e2d1ac7e79180be
grifbs <reedbandb@gmail.com>
2025-06-02 11:28:22 -0400

    Create checkbox.js

---
+++ src/components/ui/checkbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Checkbox = ({ className = '', ...props }) => {
+  return (
+    <input
+      type="checkbox"
+      className={`h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded ${className}`}
+      {...props}
+    />
+  );
+};
+
+export default Checkbox;
