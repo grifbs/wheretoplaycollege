324379b2878ba7c326dcaba2c8c57abb02931887
grifbs <reedbandb@gmail.com>
2025-06-02 11:26:42 -0400

    Update input.js

--- src/components/ui/input.js
+++ src/components/ui/input.js
@@ -1,4 +1,12 @@
-// src/components/ui/input.js
-export const Input = (props) => {
-  return <input {...props} className="border px-2 py-1 rounded w-full" />;
+import React from 'react';
+
+const Input = ({ className = '', ...props }) => {
+  return (
+    <input
+      className={`border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
+    />
+  );
 };
+
+export default Input;
